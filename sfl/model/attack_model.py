c6a72344ad81a0b72e4cc952975a143dcb1dfb03
Yang Mingxin <ymx@hust.edu.cn>
2024-01-04 15:33:39 +0800

    llama2 split

--- sfl/model/attack_model.py
+++ sfl/model/attack_model.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import Module
-from transformers import PretrainedConfig, GPT2Config
+from transformers import PretrainedConfig, GPT2Config, LlamaConfig
 
 
 class AttackModel(Module):
@@ -20,6 +20,21 @@ class GPT2AttackModel(AttackModel):
         super().__init__(config, *args, **kwargs)
         self.hidden_size = 128
         self.lstm = nn.LSTM(input_size=config.n_embd, hidden_size=self.hidden_size, batch_first=True)
+        self.mlp = nn.Linear(self.hidden_size, config.vocab_size)
+
+    def forward(self, x):
+        # x[batch_size, seq_len, n_embed]
+        # output [batch_size,seq_len, vocab_size]
+        hidden, _ = self.lstm(x)  # hidden [batch_size, seq_len, n_embed]
+        hidden = torch.dropout(hidden, p=0.1, train=self.training)
+        return self.mlp(hidden)
+    
+class LLAMA2AttackModel(AttackModel):
+
+    def __init__(self, config: LlamaConfig, *args, **kwargs):
+        super().__init__(config, *args, **kwargs)
+        self.hidden_size = 128
+        self.lstm = nn.LSTM(input_size=config.hidden_size, hidden_size=self.hidden_size, batch_first=True)
         self.mlp = nn.Linear(self.hidden_size, config.vocab_size)
 
     def forward(self, x):
---
+++ sfl/model/llama2/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['llama2_split']
